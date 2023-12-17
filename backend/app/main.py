803c9478b04c35aaf58dcd2b8c532d34555ae289
bannawandoor27 <bannawandoor@gmail.com>
2023-12-17 16:42:55 +0530

    feat: add save audio feature & websocket connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,43 @@
+import io
+import struct
+import wave
+
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from pydub import AudioSegment
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Include router from the routers module
 
 @app.websocket("/ws/audio")
 async def websocket_audio_endpoint(websocket: WebSocket):
     await websocket.accept()
+    audio_data = io.BytesIO()
+
     try:
         while True:
             data = await websocket.receive_bytes()
-            print(data)
+            audio_data.write(data)
+
     except WebSocketDisconnect:
         print("Client disconnected")
+        audio_data.seek(0)
+        wav_data = io.BytesIO()
+        with wave.open(wav_data, "wb") as wav_file:
+            wav_file.setnchannels(1)
+            wav_file.setsampwidth(2)
+            wav_file.setframerate(44100)
+            audio_data.seek(0)
+            raw_audio = audio_data.read()
+            wav_file.writeframesraw(raw_audio)
 
+        wav_data.seek(0)
+        with open("output.wav", "wb") as output_file:
+            output_file.write(wav_data.read())
--- frontend/audio_stream/src/App.jsx
+++ frontend/audio_stream/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isRecording, setIsRecording] = useState(false);
+  const [webSocket, setWebSocket] = useState(null);
+  const [audioStream, setAudioStream] = useState(null);
+
+  useEffect(() => {
+    if (isRecording) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  }, [isRecording]);
+
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    const ws = new WebSocket("ws://127.0.0.1:8000/ws/audio");
+    ws.onopen = () => {
+      console.log("Connected to WebSocket");
+    };
+    
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    setWebSocket(ws);
+    setAudioStream(stream);
+
+    const audioContext = new AudioContext();
+    const audioInput = audioContext.createMediaStreamSource(stream);
+    const bufferSize = 2048;
+    const recorder = audioContext.createScriptProcessor(bufferSize, 1, 1);
+
+    recorder.onaudioprocess = e => {
+      const data = e.inputBuffer.getChannelData(0);
+      const audioData = floatTo16BitPCM(data);
+      ws.send(audioData);
+    };
+
+    audioInput.connect(recorder);
+    recorder.connect(audioContext.destination);
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+    if (webSocket) {
+      webSocket.close();
+    }
+    if (audioStream) {
+      audioStream.getTracks().forEach(track => track.stop());
+    }
+  };
+
+  const floatTo16BitPCM = input => {
+    const output = new Int16Array(input.length);
+    for (let i = 0; i < input.length; i++) {
+      const s = Math.max(-1, Math.min(1, input[i]));
+      output[i] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+    }
+    return output;
+  };
+
+  const handleRecordToggle = () => {
+    setIsRecording(prev => !prev);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app-container">
+      <main className="main-content">
+        <h1>Real-Time Audio Streamer</h1>
+        <div className="audio-controls">
+          <button 
+            className={`record-btn ${isRecording ? 'recording' : ''}`}
+            onClick={handleRecordToggle}>
+            {isRecording ? 'Stop' : 'Start'}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
